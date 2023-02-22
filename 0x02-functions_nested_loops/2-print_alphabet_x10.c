9c4b4dbd02b3ae55b2a155127bcceca51b09ffe6
Ruvawashe <steffitanganyika@gmail.com>
2023-02-22 13:34:42 -0800

    Print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/* print alphabet 10 times - lowercase
+ * Return 0 success
+ */
+
+void print_alphabet_x10(void);
+{
+	char j;
+	int i = 0;
+
+	while (i < 10)
+	{
+
+		j = 'a'; 
+		while (j <= 'z') 
+	{
+		_putchar(j);
+		j++;
+	}
+	_putchar('\n');
+	i++;
+	}
+}
