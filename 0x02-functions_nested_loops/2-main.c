a3cf87e2f9f349db55825c386341a823dbce9acd
Ruvawashe <steffitanganyika@gmail.com>
2023-02-22 14:41:34 -0800

    slower

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,7 +1,12 @@
-#include <main.h>
-int main()
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	print_alphabet_x10();
-	return 0;
-}
-		
+    print_alphabet_x10();
+    return (0);
+}		
