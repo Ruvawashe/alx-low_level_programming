ebc0ad8eb068d0ff7f55e61c2034c6303e507a82
Ruvawashe <steffitanganyika@gmail.com>
2023-02-23 12:45:32 -0800

    formula that checks for uppercase characters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <main.h>
 
 /**
@@ -10,10 +9,10 @@
 int _isupper(int c)
 {	
 
-	if (c = 'A'; , c <= 'Z;')
-	       return (1)
+	if (c >= 'A'; && c <= 'Z;')
+	       return (1);
 
-	return (0)	       
+	return (0);	       
 
 
 
