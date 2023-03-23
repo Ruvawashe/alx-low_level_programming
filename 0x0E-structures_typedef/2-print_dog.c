0b34933a6c39ba561f4bff6fca1379701c9ece6c
Ruvawashe <steffitanganyika@gmail.com>
2023-03-23 08:00:02 -0700

    new commit on print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  *print_dog - a function that prints a struct dog
@@ -16,8 +16,6 @@ if (d->name == NULL)
 d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
-if (d->age == NULL)
-d->age = "(nil)";
 
 printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
