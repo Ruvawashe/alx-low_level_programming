5a6e8655af25abc4328cf470b2669d4affa0b79e
Ruvawashe <steffitanganyika@gmail.com>
2023-03-23 05:32:24 -0700

    new struct called dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,3 +6,15 @@ struct dog
 	float age;
 	char *owner;
 };
+
+int main(void)
+{
+	struct dog dog;
+
+	dog.name = "Love";
+	dog.age = "3";
+	dog.owner = "Steffi";
+	printf("My name is %s, and I am %1.f years old.\n, I am owned by %s\n", dog.name, dog.age, dog.owner);
+	return (0);
+}
+
