870d1058f9b940d7ed31e2ea5be9abc9dc162c47
Ruvawashe <steffitanganyika@gmail.com>
2023-03-23 07:11:40 -0700

    struct dog variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
+
+/**
+ * declaration of struct dog in header file dog.h
+ * @name - gives the name of dog
+ * @age - gives age of dog
+ * @owner - gives name of owner of dog
+ * return 
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 {
