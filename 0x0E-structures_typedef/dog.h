15929d837b2fccd08737e86a207a8d8725c96f19
Ruvawashe <steffitanganyika@gmail.com>
2023-03-23 06:16:30 -0700

    new struct dog commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,20 @@
-#include <stdio.h>
+#ifndef _DOG_H_
+#define _DOG_H_
 
+/**
+ *declaring a header file
+ *contains file prototype
+ *declare header files
+ */
 struct dog 
 { 
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t; 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
